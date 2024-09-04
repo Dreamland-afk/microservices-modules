06b5de0cfb4c7de9d6d9c236e3b8b4b32091074a
swapnadeepm <swapnadeep.mondal@e-arc.com>
2024-09-04 13:13:58 +0530

    Added Cards module files

--- cards/src/main/java/com/dreamquest/cards/service/impl/CardsServiceImpl.java
+++ cards/src/main/java/com/dreamquest/cards/service/impl/CardsServiceImpl.java
@@ -1,22 +1,48 @@
 package com.dreamquest.cards.service.impl;
 
+import com.dreamquest.cards.Constants.CardsConstants;
 import com.dreamquest.cards.dto.CardsDto;
+import com.dreamquest.cards.entity.Cards;
+import com.dreamquest.cards.exception.CardsAlreadyExistsException;
 import com.dreamquest.cards.repository.CardsRepository;
 import com.dreamquest.cards.service.ICardsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
 public class CardsServiceImpl implements ICardsService {
 
     CardsRepository cardsRepository;
+
+
     /**
      * @param mobileNumber - Mobile Number of the Customer
      */
     @Override
     public void createCard(String mobileNumber) {
 
+        Cards cards = cardsRepository.findByMobileNumber(mobileNumber).orElseThrow(
+
+                () -> new CardsAlreadyExistsException("Card already registered with given mobileNumber " + mobileNumber)
+        );
+
+        cardsRepository.save(createNewCards(mobileNumber));
+    }
+
+    private Cards createNewCards(String mobileNumber) {
+
+        Cards newCard = new Cards();
+        long randomCardNumber = 100000000000L + new Random().nextInt(900000000);
+        newCard.setCardNumber(Long.toString(randomCardNumber));
+        newCard.setMobileNumber(mobileNumber);
+        newCard.setCardType(CardsConstants.CREDIT_CARD);
+        newCard.setTotalLimit(CardsConstants.NEW_CARD_LIMIT);
+        newCard.setAmountUsed(0);
+        newCard.setAvailableAmount(CardsConstants.NEW_CARD_LIMIT);
+        return newCard;
     }
 
     /**
