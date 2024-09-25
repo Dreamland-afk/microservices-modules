f2ed6cace8b17bea6a6d1ff36bad0a600a6f28eb
swapnadeepm <swapnadeep.mondal@e-arc.com>
2024-09-25 17:32:23 +0530

    Initial commit

--- Api Gateway/apigateway/src/main/java/com/dreamquest/apigateway/filters/LoggingGlobalFiltersConfigurations.java
+++ Api Gateway/apigateway/src/main/java/com/dreamquest/apigateway/filters/LoggingGlobalFiltersConfigurations.java
@@ -6,9 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Component
 public class LoggingGlobalFiltersConfigurations implements GlobalFilter {
     private static final Logger logger = LoggerFactory.getLogger(LoggingGlobalFiltersConfigurations.class);
 
