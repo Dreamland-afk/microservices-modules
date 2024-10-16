94d6d5ac456623260ecc9fc32d674130bde8ee3d
swapnadeepm <swapnadeep.mondal@e-arc.com>
2024-10-16 13:34:35 +0530

    mn

---
+++ Event Driven MC/messageService/src/main/java/com/dreamquest/messageService/functions/MessageService.java
@@ -0,0 +1,57 @@
+package com.dreamquest.messageService.functions;
+
+import com.dreamquest.messageService.dto.AccountMsgDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+@Configuration
+public class MessageService {
+
+    @Bean
+    public Supplier<Mono<Void>> sms() {
+        return () -> {
+            System.out.println("SMS sent successfully");
+            return Mono.empty();  // Return an empty Mono<Void> instead of null
+        };
+    }
+
+    @Bean
+    public Supplier<Mono<Void>> multisms() {
+        return () -> {
+            System.out.println("SMS sent successfully from second sms");
+            return Mono.empty();  // Return an empty Mono<Void> instead of null
+        };
+    }
+
+
+    @Bean
+    public Function<AccountMsgDto,AccountMsgDto> email()
+    {
+
+       return  accountMsgDto -> {
+
+           System.out.println("Sending email to: " + accountMsgDto.email());
+           return  accountMsgDto;
+       };
+
+    }
+
+    @Bean
+    public Function<AccountMsgDto,String> abc()
+    {
+
+        return  accountMsgDto -> {
+
+            System.out.println("Sending email to: " + accountMsgDto.email());
+            return  accountMsgDto.email();
+        };
+
+    }
+
+}
+
